852574a15da8c5f96c696afd3560a2f9e963e406
Oleksandr Yavorskyi <55851245+slimnation23@users.noreply.github.com>
2026-09-14 16:02:14 +0200

    Center empty state icon

--- script.js
+++ script.js
@@ -291,7 +291,7 @@ async function fetchSanityData() {
       if (wrapper) {
         wrapper.innerHTML = `
           <div class="swiper-slide !w-full flex flex-col items-center justify-center p-8 lg:p-16 bg-[#EAE8E4] rounded-2xl border-2 border-dashed border-gray-400 text-black">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 mb-4 opacity-50" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 mx-auto mb-4 opacity-50" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
             <h3 class="text-2xl lg:text-4xl font-ermilov uppercase text-center mb-4">Нові події готуються</h3>
